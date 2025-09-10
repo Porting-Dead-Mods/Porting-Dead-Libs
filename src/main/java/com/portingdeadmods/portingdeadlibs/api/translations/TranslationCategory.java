f9d564b5db1cafd942772965f02e9eb7be6effb5
thepigcat <benpospo@gmail.com>
2025-09-10 13:12:31 +0200

    v1.0.38

--- src/main/java/com/portingdeadmods/portingdeadlibs/api/translations/TranslationCategory.java
+++ src/main/java/com/portingdeadmods/portingdeadlibs/api/translations/TranslationCategory.java
@@ -3,7 +3,7 @@ package com.portingdeadmods.portingdeadlibs.api.translations;
 public record TranslationCategory(DeferredTranslationRegister register, String category) {
     public DeferredTranslation<TranslatableConstant> registerWithDefault(String key, String defaultTranslation) {
         DeferredTranslation<TranslatableConstant> translation = this.register.register(key, () -> new TranslatableConstant(key, category));
-        register.getDefaultTranslations().put(category + register.getNamespace() + key, defaultTranslation);
+        register.getDefaultTranslations().put(category + "." + register.getNamespace() + "." + key, defaultTranslation);
         return translation;
     }
 }
