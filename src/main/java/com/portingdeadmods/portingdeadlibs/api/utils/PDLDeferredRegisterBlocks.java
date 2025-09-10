f43b992933744d5c70c268727f7186ab87ad19bb
thepigcat <benpospo@gmail.com>
2025-09-10 11:40:44 +0200

    v1.0.34

---
+++ src/main/java/com/portingdeadmods/portingdeadlibs/api/utils/PDLDeferredRegisterBlocks.java
@@ -0,0 +1,61 @@
+package com.portingdeadmods.portingdeadlibs.api.utils;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class PDLDeferredRegisterBlocks extends DeferredRegister.Blocks {
+    private final DeferredRegister.Items itemsRegistry;
+    private final List<Supplier<? extends BlockItem>> blockItems;
+
+    protected PDLDeferredRegisterBlocks(String namespace, DeferredRegister.Items itemsRegistry) {
+        super(namespace);
+        this.itemsRegistry = itemsRegistry;
+        this.blockItems = new ArrayList<>();
+    }
+
+    public <B extends Block> DeferredBlock<B> registerWithItem(String name, Supplier<? extends B> sup) {
+        DeferredBlock<B> block = this.register(name, key -> sup.get());
+        DeferredItem<BlockItem> item = this.itemsRegistry.registerSimpleBlockItem(name, block, new Item.Properties());
+        this.blockItems.add(item);
+        return block;
+    }
+
+    public <B extends Block, BI extends BlockItem> DeferredBlock<B> registerWithItem(String name, Supplier<? extends B> sup, Function<B, BI> biFunction) {
+        DeferredBlock<B> block = this.register(name, key -> sup.get());
+        DeferredItem<BI> item = this.itemsRegistry.register(name, () -> biFunction.apply(block.get()));
+        this.blockItems.add(item);
+        return block;
+    }
+
+    public <B extends Block> DeferredBlock<B> registerBlockWithItem(String name, Function<BlockBehaviour.Properties, ? extends B> func, BlockBehaviour.Properties props) {
+        return this.registerWithItem(name, (() -> func.apply(props)));
+    }
+
+    public <B extends Block> DeferredBlock<B> registerBlockWithItem(String name, Function<BlockBehaviour.Properties, ? extends B> func) {
+        return this.registerBlockWithItem(name, func, BlockBehaviour.Properties.of());
+    }
+
+    public DeferredBlock<Block> registerSimpleBlockWithItem(String name, BlockBehaviour.Properties props) {
+        return this.registerBlockWithItem(name, Block::new, props);
+    }
+
+    public DeferredBlock<Block> registerSimpleBlockWithItem(String name) {
+        return this.registerSimpleBlockWithItem(name, BlockBehaviour.Properties.of());
+    }
+
+    public List<Supplier<? extends BlockItem>> getBlockItems() {
+        return blockItems;
+    }
+
+}
---
+++ src/main/java/com/portingdeadmods/portingdeadlibs/api/utils/PDLDeferredRegisterItems.java
@@ -0,0 +1,78 @@
+package com.portingdeadmods.portingdeadlibs.api.utils;
+
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class PDLDeferredRegisterItems extends DeferredRegister.Items {
+    private final List<Supplier<? extends Item>> creativeTabItems;
+
+    protected PDLDeferredRegisterItems(String namespace) {
+        super(namespace);
+        this.creativeTabItems = new ArrayList<>();
+    }
+
+    @Override
+    public <I extends Item> DeferredItem<I> register(String name, Function<ResourceLocation, ? extends I> func) {
+        DeferredItem<I> item = super.register(name, func);
+        this.creativeTabItems.add(item);
+        return item;
+    }
+
+    public <I extends Item> DeferredItem<I> registerNoCreative(String name, Function<ResourceLocation, ? extends I> func) {
+        return super.register(name, func);
+    }
+
+    public <I extends Item> DeferredItem<I> registerNoCreative(String name, Supplier<? extends I> sup) {
+        return this.registerNoCreative(name, key -> sup.get());
+    }
+
+    public DeferredItem<BlockItem> registerSimpleBlockItemNoCreative(String name, Supplier<? extends Block> block, Item.Properties properties) {
+        return this.registerNoCreative(name, key -> new BlockItem(block.get(), properties));
+    }
+
+    public DeferredItem<BlockItem> registerSimpleBlockItemNoCreative(String name, Supplier<? extends Block> block) {
+        return this.registerSimpleBlockItemNoCreative(name, block, new Item.Properties());
+    }
+
+    public DeferredItem<BlockItem> registerSimpleBlockItemNoCreative(Holder<Block> block, Item.Properties properties) {
+        String var10001 = block.unwrapKey().orElseThrow().location().getPath();
+        Objects.requireNonNull(block);
+        return this.registerSimpleBlockItemNoCreative(var10001, block::value, properties);
+    }
+
+    public DeferredItem<BlockItem> registerSimpleBlockItemNoCreative(Holder<Block> block) {
+        return this.registerSimpleBlockItemNoCreative(block, new Item.Properties());
+    }
+
+    public <I extends Item> DeferredItem<I> registerItemNoCreative(String name, Function<Item.Properties, ? extends I> func, Item.Properties props) {
+        return this.registerNoCreative(name, () -> func.apply(props));
+    }
+
+    public <I extends Item> DeferredItem<I> registerItemNoCreative(String name, Function<Item.Properties, ? extends I> func) {
+        return this.<I>registerItemNoCreative(name, func, new Item.Properties());
+    }
+
+    public DeferredItem<Item> registerSimpleItemNoCreative(String name, Item.Properties props) {
+        return this.<Item>registerItemNoCreative(name, Item::new, props);
+    }
+
+    public DeferredItem<Item> registerSimpleItemNoCreative(String name) {
+        return this.<Item>registerItemNoCreative(name, Item::new, new Item.Properties());
+    }
+
+    public List<Supplier<? extends Item>> getCreativeTabItems() {
+        return creativeTabItems;
+    }
+}
